70d89e1e2c303681a15f061108c735e5bb130f90
Nesiro81 <rejebnesiro81@gmail.com>
2024-12-08 21:22:51 +0100

    update url

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // backend locally using express server on port 5000
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  // render hosted backend api
+  baseURL: "https://amazon-backend-react-api.onrender.com",
 });
 
 export { axiosInstance };
